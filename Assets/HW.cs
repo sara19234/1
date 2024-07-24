e5a967c738342b818b6a70604eb0f4b976ebf680
sara19234 <sara.spencer@studytafensw.edu.au>
2024-07-24 14:21:55 +1000

    initial commit

---
+++ Assets/HW.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HW : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello this is the start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello this is the start");
+    }
+}
